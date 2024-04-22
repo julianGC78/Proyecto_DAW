edf34113c99985d7dbe6c815eb45a03390b7a519
juliangc24 <juliangc24@educastur.es>
2024-04-22 20:09:11 +0200

    Commit inicial

---
+++ src/main/java/paixel/PaixelJwtApplication.java
@@ -0,0 +1,13 @@
+package paixel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaixelJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaixelJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paixel/PaixelJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package paixel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaixelJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
